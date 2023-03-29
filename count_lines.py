f9b0b33e277aa252d3ede774c0f0ab92d7c0e2cb
michalszulowski <michal.szulows@gmail.com>
2023-03-29 16:57:36 +0200

    Finish basic version

--- count_lines.py
+++ count_lines.py
@@ -1,35 +1,57 @@
 import os
-import json
 import sys
-import codecs
 
-excluded_words = ['cmake', '/.', '.out', 'target/', 'doc/']
+excluded_words_path = 'excluded_words.txt'
+extensions_path = 'extensions.txt'
+
+
+def read_lines(path):
+    file = open(path, 'r')
+    lines = file.read().splitlines()
+    file.close()
+    return lines
+
+
+extensions = read_lines(extensions_path)
+excluded_words = read_lines(excluded_words_path)
+
 
 def get_sum_of_lines(path):
     files = os.listdir(path)
     if len(files) == 0:
         return 0
     sum_of_lines = 0
-    for f in files:
-        full_path = path + '/' + f
-        if does_file_meet_conditions(full_path):
-            if os.path.isdir(full_path):
-                sum_of_lines += get_sum_of_lines(full_path)
-            else:
+    for file in files:
+        full_path = path + '/' + file
+        if os.path.isdir(full_path):
+            sum_of_lines += get_sum_of_lines(full_path)
+        else:
+            if does_file_meet_conditions(full_path):
                 print(full_path)
                 sum_of_lines += sum(1 for line in open(full_path))
     
     return sum_of_lines
 
+
 def does_file_meet_conditions(path):
+    file_name, extension = os.path.splitext(path)
+    if extension not in extensions:
+        return False
     for word in excluded_words:
-        if word in path:
+        if word in file_name:
             return False
     return True
 
-def is_on_windows():
-    return os.name == 'nt'
 
-#print(os.listdir('./util'))
-number_of_lines = get_sum_of_lines('.')
-print(number_of_lines)
+def get_paths_from_args():
+    paths = []
+    for i in range(1, len(sys.argv)):
+        paths.append(sys.argv[i])
+    return paths
+
+
+counted_dirs = get_paths_from_args()
+total_sum = 0
+for directory in counted_dirs:
+    total_sum += get_sum_of_lines(directory)
+print(total_sum)
