8de985dc9116f9b875599d2c37fafff05dd1f4d3
Michał Szulowski <michal.szulows@gmail.com>
2023-03-30 22:12:33 +0200

    Improve installer

--- install-line-counter.sh
+++ install-line-counter.sh
@@ -2,10 +2,14 @@
 
 LINE_COUNTER_SRC_DIR="/usr/local/src/line-counter/"
 
+# install script files
+mkdir $LINE_COUNTER_SRC_DIR
 cd $LINE_COUNTER_SRC_DIR 
+
 wget https://github.com/michalszulowski/line-counter/blob/main/count_lines.py
 wget https://github.com/michalszulowski/line-counter/blob/main/extensions.txt
 wget https://github.com/michalszulowski/line-counter/blob/main/excluded_words.txt
 
+# add bash command
 cd /usr/bin/
-
+wget https://github.com/michalszulowski/line-counter/blob/main/count-lines
